449cd60452e2667b66a2aea2b956fa174da230e7
Zbynek Muzik <zbynek.muzik@hypersonica.com>
2015-12-15 12:42:22 +0700

    divi update

--- export_for_build.sh
+++ export_for_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp zb@185.8.238.141:/home/zb/csapi/api/data.db ./
 
-sqlite3 data.db 'select * from dividend;' > ../app/src/main/assets/dividend.csv
-sqlite3 data.db 'select * from historical_quote;' > ../app/src/main/assets/historical_quote.csv
-sqlite3 data.db 'select * from stock_detail;' > ../app/src/main/assets/stock_detail.csv
+sqlite3 data.db 'select * from dividend;' > ../cs/app/src/main/assets/dividend.csv
+sqlite3 data.db 'select * from historical_quote;' > ../cs/app/src/main/assets/historical_quote.csv
+sqlite3 data.db 'select * from stock_detail;' > ../cs/app/src/main/assets/stock_detail.csv
